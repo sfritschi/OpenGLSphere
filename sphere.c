09c7cf6caaaa96c298a227fb3084491c5ac8b376
severinf <severin.f@hotmail.ch>
2022-02-16 22:49:02 +0100

    Added normals

--- sphere.c
+++ sphere.c
@@ -8,23 +8,27 @@
 #include <math.h>
 
 #define MAX_INFO 512
-#define N_VERTICES 8
 
 // Source of vertex shader in glsl
 const GLchar *vertexShaderSource = R"glsl(
 #version 460 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 3) in vec3 aNormal;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
 out vec3 vertexColor;
+out vec3 vertexNormal;
+out vec3 fragPos;
 
 void main() {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
+    fragPos = vec3(model * vec4(aPos, 1.0));
     vertexColor = aColor;
+    vertexNormal = aNormal;
 }
 )glsl";
 
@@ -34,6 +38,8 @@ const GLchar *fragmentShaderSource = R"glsl(
 out vec4 fragColor;
 
 in vec3 vertexColor;
+in vec3 vertexNormal;
+in vec3 fragPos;
 
 void main() {
     fragColor = vec4(vertexColor, 1.0);
@@ -44,6 +50,7 @@ void main() {
 typedef struct {
 	vec3 pos;
 	vec3 col;
+	vec3 normal;
 } Vertex;
 
 // GLOBALS
@@ -191,18 +198,38 @@ int main(int argc, char *argv[]) {
 	glClearColor(0.8, 0.8, 0.8, 1.0);
 	glEnable(GL_DEPTH_TEST);
 	
-	// Initialize data
-	Vertex vertices[N_VERTICES] = {
-		// Front face (green)
-		(Vertex){.pos = {-0.5f, -0.5f, -1.0f}, .col = {0.1f, 0.8f, 0.6f}},
-		(Vertex){.pos = {0.5f, -0.5f, -1.0f}, .col = {0.1f, 0.8f, 0.6f}},
-		(Vertex){.pos = {0.5f, 0.5f, -1.0f}, .col = {0.1f, 0.8f, 0.6f}},
-		(Vertex){.pos = {-0.5f, 0.5f, -1.0f}, .col = {0.1f, 0.8f, 0.6f}},
-		// Back face (blue)
-		(Vertex){.pos = {-0.5f, -0.5f, -2.0f}, .col = {0.1f, 0.8f, 0.6f}},
-		(Vertex){.pos = {0.5f, -0.5f, -2.0f}, .col = {0.1f, 0.8f, 0.6f}},
-		(Vertex){.pos = {0.5f, 0.5f, -2.0f}, .col = {0.1f, 0.8f, 0.6f}},
-		(Vertex){.pos = {-0.5f, 0.5f, -2.0f}, .col = {0.1f, 0.8f, 0.6f}}
+	// Initialize vertex data
+	Vertex vertices[] = {
+		// Front face (red)
+		(Vertex){.pos = {-0.5f, -0.5f, -1.0f}, .col = {1.0f, 0.0f, 0.0f}, .normal = {0.0f, 0.0f, 1.0f}},
+		(Vertex){.pos = {0.5f, -0.5f, -1.0f}, .col = {1.0f, 0.0f, 0.0f}, .normal = {0.0f, 0.0f, 1.0f}},
+		(Vertex){.pos = {0.5f, 0.5f, -1.0f}, .col = {1.0f, 0.0f, 0.0f}, .normal = {0.0f, 0.0f, 1.0f}},
+		(Vertex){.pos = {-0.5f, 0.5f, -1.0f}, .col = {1.0f, 0.0f, 0.0f}, .normal = {0.0f, 0.0f, 1.0f}},
+		// Back face (green)
+		(Vertex){.pos = {-0.5f, -0.5f, -2.0f}, .col = {0.0f, 1.0f, 0.0f}, .normal = {0.0f, 0.0f, -1.0f}},
+		(Vertex){.pos = {0.5f, -0.5f, -2.0f}, .col = {0.0f, 1.0f, 0.0f}, .normal = {0.0f, 0.0f, -1.0f}},
+		(Vertex){.pos = {0.5f, 0.5f, -2.0f}, .col = {0.0f, 1.0f, 0.0f}, .normal = {0.0f, 0.0f, -1.0f}},
+		(Vertex){.pos = {-0.5f, 0.5f, -2.0f}, .col = {0.0f, 1.0f, 0.0f}, .normal = {0.0f, 0.0f, -1.0f}},
+		// Bottom face (blue)
+		(Vertex){.pos = {-0.5f, -0.5f, -1.0f}, .col = {0.0f, 0.0f, 1.0f}, .normal = {0.0f, -1.0f, 0.0f}},
+		(Vertex){.pos = {0.5f, -0.5f, -1.0f}, .col = {0.0f, 0.0f, 1.0f}, .normal = {0.0f, -1.0f, 0.0f}},
+		(Vertex){.pos = {0.5f, -0.5f, -2.0f}, .col = {0.0f, 0.0f, 1.0f}, .normal = {0.0f, -1.0f, 0.0f}},
+		(Vertex){.pos = {-0.5f, -0.5f, -2.0f}, .col = {0.0f, 0.0f, 1.0f}, .normal = {0.0f, -1.0f, 0.0f}},
+		// Top face (yellow)
+		(Vertex){.pos = {-0.5f, 0.5f, -1.0f}, .col = {1.0f, 1.0f, 0.0f}, .normal = {0.0f, 1.0f, 0.0f}},
+		(Vertex){.pos = {0.5f, 0.5f, -1.0f}, .col = {1.0f, 1.0f, 0.0f}, .normal = {0.0f, 1.0f, 0.0f}},
+		(Vertex){.pos = {0.5f, 0.5f, -2.0f}, .col = {1.0f, 1.0f, 0.0f}, .normal = {0.0f, 1.0f, 0.0f}},
+		(Vertex){.pos = {-0.5f, 0.5f, -2.0f}, .col = {1.0f, 1.0f, 0.0f}, .normal = {0.0f, 1.0f, 0.0f}},
+		// Left face (magenta)
+		(Vertex){.pos = {-0.5f, -0.5f, -2.0f}, .col = {1.0f, 0.0f, 1.0f}, .normal = {-1.0f, 0.0f, 0.0f}},
+		(Vertex){.pos = {-0.5f, -0.5f, -1.0f}, .col = {1.0f, 0.0f, 1.0f}, .normal = {-1.0f, 0.0f, 0.0f}},
+		(Vertex){.pos = {-0.5f, 0.5f, -1.0f}, .col = {1.0f, 0.0f, 1.0f}, .normal = {-1.0f, 0.0f, 0.0f}},
+		(Vertex){.pos = {-0.5f, 0.5f, -2.0f}, .col = {1.0f, 0.0f, 1.0f}, .normal = {-1.0f, 0.0f, 0.0f}},
+		// Right face (cyan)
+		(Vertex){.pos = {0.5f, -0.5f, -2.0f}, .col = {0.0f, 1.0f, 1.0f}, .normal = {1.0f, 0.0f, 0.0f}},
+		(Vertex){.pos = {0.5f, -0.5f, -1.0f}, .col = {0.0f, 1.0f, 1.0f}, .normal = {1.0f, 0.0f, 0.0f}},
+		(Vertex){.pos = {0.5f, 0.5f, -1.0f}, .col = {0.0f, 1.0f, 1.0f}, .normal = {1.0f, 0.0f, 0.0f}},
+		(Vertex){.pos = {0.5f, 0.5f, -2.0f}, .col = {0.0f, 1.0f, 1.0f}, .normal = {1.0f, 0.0f, 0.0f}}
 	};
 	
 	// Triangle indices
@@ -214,17 +241,17 @@ int main(int argc, char *argv[]) {
 		4, 5, 6,
 		4, 6, 7,
 		// Bottom face
-		0, 1, 5,
-		0, 5, 4,
+		8, 9, 10,
+		8, 10, 11,
 		// Top face
-		3, 2, 6,
-		3, 6, 7,
+		12, 13, 14,
+		12, 14, 15,
 		// Left face
-		4, 0, 3,
-		4, 3, 7,
+		16, 17, 18,
+		16, 18, 19,
 		// Right face
-		1, 5, 6,
-		1, 6, 2
+		20, 21, 22,
+		20, 22, 23
 	};
 	// Set total number of indices
 	nIndices = sizeof(indices) / sizeof(GLushort);
@@ -277,9 +304,11 @@ int main(int argc, char *argv[]) {
 	GLsizeiptr size = 3;  // size of individual attribute (3D)
 	GLsizeiptr stride = sizeof(Vertex);
 	// Enable vertex position attribute
-	enableVertAttrib(vboVert, size, stride, 0, (void *)0);
+	enableVertAttrib(vboVert, size, stride, 0, (void *)offsetof(Vertex, pos));
 	// Enable vertex color attribute
 	enableVertAttrib(vboVert, size, stride, 1, (void *)offsetof(Vertex, col));
+	// Enable vertex normal attribute
+	enableVertAttrib(vboVert, size, stride, 2, (void *)offsetof(Vertex, normal));
 	
 	// Start using shader program
 	glUseProgram(shaderProgram);
